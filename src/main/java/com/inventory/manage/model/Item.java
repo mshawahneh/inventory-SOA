584fb939ee5d202843435c36cc4a61e0c395be41
Noura Hoshieah <noura.hoshieah.1@gmail.com>
2017-05-07 11:29:05 +0300

    Update Item.java

--- src/main/java/com/inventory/manage/model/Item.java
+++ src/main/java/com/inventory/manage/model/Item.java
@@ -15,5 +15,91 @@ public class Item implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private String itemName;
+	private int itemColor;
+	private char itemSymbol;
+	private int itemWeight;
+   	private int itemValue;
+    
+    
+	public Item() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
+
+	public Item(String itemName, int itemColor, char itemSymbol,
+			int itemWeight, int itemValue) {
+		super();
+		this.itemName = itemName;
+		this.itemColor = itemColor;
+		this.itemSymbol = itemSymbol;
+		this.itemWeight = itemWeight;
+		this.itemValue = itemValue;
+	}
+
+
+	public String getItemName() {
+		return itemName;
+	}
+
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+
+	public int getItemColor() {
+		return itemColor;
+	}
+
+
+	public void setItemColor(int itemColor) {
+		this.itemColor = itemColor;
+	}
+
+
+	public char getItemSymbol() {
+		return itemSymbol;
+	}
+
+
+	public void setItemSymbol(char itemSymbol) {
+		this.itemSymbol = itemSymbol;
+	}
+
+
+	public int getItemWeight() {
+		return itemWeight;
+	}
+
+
+	public void setItemWeight(int itemWeight) {
+		this.itemWeight = itemWeight;
+	}
+
+
+	public int getItemValue() {
+		return itemValue;
+	}
+
+
+	public void setItemValue(int itemValue) {
+		this.itemValue = itemValue;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Item [itemName=" + itemName + ", itemColor=" + itemColor
+				+ ", itemSymbol=" + itemSymbol + ", itemWeight=" + itemWeight
+				+ ", itemValue=" + itemValue + "]";
+	}
+	
+	
+    
+    
+    
+    
 }
+
