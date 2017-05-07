a64fd4f41f2d893c730d0b858e62f8baa88deb7d
Noura Hoshieah <noura.hoshieah.1@gmail.com>
2017-05-07 11:18:12 +0300

    Update Customer.java

--- src/main/java/com/inventory/manage/model/Customer.java
+++ src/main/java/com/inventory/manage/model/Customer.java
@@ -4,17 +4,67 @@
 package com.inventory.manage.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
- * @author mshawahn
+ * @author 
  *
  */
 public class Customer implements Serializable{
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+
+	
+	private String CustomerID;
+	private String CustomerFirstName;
+	private String CustomerLastName;
+	private Date CustomerBirthDate;
+	
+	
+	public Customer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Customer(String customerID, String customerFirstName,
+			String customerLastName, Date customerBirthDate) {
+		super();
+		CustomerID = customerID;
+		CustomerFirstName = customerFirstName;
+		CustomerLastName = customerLastName;
+		CustomerBirthDate = customerBirthDate;
+	}
+	public String getCustomerID() {
+		return CustomerID;
+	}
+	public void setCustomerID(String customerID) {
+		CustomerID = customerID;
+	}
+	public String getCustomerFirstName() {
+		return CustomerFirstName;
+	}
+	public void setCustomerFirstName(String customerFirstName) {
+		CustomerFirstName = customerFirstName;
+	}
+	public String getCustomerLastName() {
+		return CustomerLastName;
+	}
+	public void setCustomerLastName(String customerLastName) {
+		CustomerLastName = customerLastName;
+	}
+	public Date getCustomerBirthDate() {
+		return CustomerBirthDate;
+	}
+	public void setCustomerBirthDate(Date customerBirthDate) {
+		CustomerBirthDate = customerBirthDate;
+	}
+	@Override
+	public String toString() {
+		return "Customer [CustomerID=" + CustomerID + ", CustomerFirstName="
+				+ CustomerFirstName + ", CustomerLastName=" + CustomerLastName
+				+ ", CustomerBirthDate=" + CustomerBirthDate + "]";
+	}
+	
+	
+	
 	
 
 }
