bab1efad0e03fd9d35843327d509028e069e37c6
Noura Hoshieah <noura.hoshieah.1@gmail.com>
2017-05-11 00:14:17 +0300

    Update PaymentService.java

--- src/main/java/com/inventory/manage/service/payment/PaymentService.java
+++ src/main/java/com/inventory/manage/service/payment/PaymentService.java
@@ -3,10 +3,41 @@
  */
 package com.inventory.manage.service.payment;
 
+import java.util.List;
+
+import com.inventory.manage.model.Payment;
+
 /**
- * @author mshawahn
+ * @author Noura
  *
  */
-public interface PaymentService {
+public interface PaymentsService {
 
+	/**
+	 * Add new payment
+	 * @param payment
+	 * @return An Payment
+	 */
+	public Payment addPayment(Payment payment);
+	
+	/**
+	 * 
+	 * @param date
+	 * @return
+	 */
+	public boolean isExist(String name);
+	
+	/**
+	 * Get all payments
+	 * @return
+	 * @throws DAOException
+	 */
+    public List<Payment> getAllPayments();
+
+    /**
+     * Delete an Payment
+     * @param payment
+     * @return
+     */
+	public boolean delete(Payment payment);
 }
