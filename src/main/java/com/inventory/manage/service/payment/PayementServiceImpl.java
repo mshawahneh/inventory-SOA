507d20d464e48b877db8a24b00a7a1e2ae3d6ec8
Noura Hoshieah <noura.hoshieah.1@gmail.com>
2017-05-11 00:16:42 +0300

    Update PayementServiceImpl.java

--- src/main/java/com/inventory/manage/service/payment/PayementServiceImpl.java
+++ src/main/java/com/inventory/manage/service/payment/PayementServiceImpl.java
@@ -3,20 +3,47 @@
  */
 package com.inventory.manage.service.payment;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.inventory.manage.model.Payment;
 import com.inventory.manage.repositories.PaymentRepository;
 
 /**
- * @author mshawahn
+ * @author Noura
  *
  */
-@Service("payementService")
+@Service("paymentsService")
 @Transactional
-public class PayementServiceImpl implements PaymentService {
-	
+public class PaymentsServiceImpl implements PaymentsService {
+
 	@Autowired
 	private PaymentRepository paymentRepository;
+
+	@Override
+	public Payment addPayment(Payment payment) {
+		// TODO To be implemented
+		return null;
+	}
+
+	@Override
+	public boolean isExist(String name) {
+		// TODO To be implemented
+		return false;
+	}
+
+	@Override
+	public List<Payment> getAllPayments() {
+		// TODO To be implemented
+		return null;
+	}
+
+	@Override
+	public boolean delete(Payment payment) {
+		// TODO To be implemented
+		return false;
+	}
 }
